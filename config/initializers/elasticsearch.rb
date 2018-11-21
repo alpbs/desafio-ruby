fd5bba9461fbd29031f48d5a0ffbf84f319f7892
André Barros <alpbs2003@gmail.com>
2018-11-21 07:45:35 -0200

    elasticsearch for heroku

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,12 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new({
-  log: true
-})
+unless ENV['RAILS_ENV'] == 'production'
+  Elasticsearch::Model.client = Elasticsearch::Client.new({
+    log: true
+  })
+else 
+  Elasticsearch::Model.client = Elasticsearch::Client.new({
+    host: 'apple-934232035.us-east-1.bonsaisearch.net',
+    user: 'cvs5jfw6ly',
+    password: '9dlb5sk8lg',
+    log: true
+  })
+end
